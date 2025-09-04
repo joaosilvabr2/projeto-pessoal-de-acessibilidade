939430d04944f1b32f061709304f1f5f925943da
joaosilvabr2 <00001088488961sp@al.educacao.sp.gov.br>
2025-09-04 16:18:27 -0300

    Add files via upload

---
+++ site-acessivel-projeto-inicial/script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function(){
+   const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+   const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
+   botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+   })
+
+   const aumentarFonteBotao = document.getElementById('aumentar-fonte');
+   const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+
+   let tamanhoAtualFonte =1;
+
+   aumentarFonteBotao.addEventListener('click', function(){
+       tamanhoAtualFonte += 0.1;
+       document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+   })
+
+    diminuirFonteBotaoFonteBotao.addEventListener('click', function(){
+       tamanhoAtualFonte -= 0.1;
+       document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+   })
+
+
+})
